86408cdba6c349558a2bc530055ed9aaebe24984
Weil <imweilbyte@gmail.com>
2019-01-21 00:22:16 +0100

    Updated project files

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RimRPC")]
-[assembly: AssemblyCopyright("Copyright © NeonTrash 2018")]
+[assembly: AssemblyCopyright("Copyright © Weilbyte 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
